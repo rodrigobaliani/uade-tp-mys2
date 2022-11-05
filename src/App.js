8bc4223f10c67ce162977dda0fb0eb4e59dc1db3
Rodrigo Baliani <rodro96@gmail.com>
2022-11-05 16:48:52 -0300

    Grafico con react-vis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Grid, Typography, Card, CardContent } from '@mui/material';
+import { HorizontalGridLines, LineSeries, makeWidthFlexible, VerticalGridLines, XAxis, XYPlot, YAxis } from "react-vis";
+import * as math from 'mathjs';
+
+const X_VALUES_COUNT = 50;
+const FlexibleXYPlot = makeWidthFlexible(XYPlot);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography style={{ textAlign: 'center', color: 'black' }} variant='h4'>
+            Integración
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Card>
+            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+              <FlexibleXYPlot height={600}>
+                <VerticalGridLines />
+                <HorizontalGridLines />
+                <XAxis />
+                <YAxis />
+                <LineSeries
+                  color="red"
+                  data={[
+                    { x: 1, y: 10 },
+                    { x: 2, y: 5 },
+                    { x: 3, y: 15 }
+                  ]} />
+              </FlexibleXYPlot>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container >
   );
 }
+
 
 export default App;
