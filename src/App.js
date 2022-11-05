1313ac47da6f04b2345d7649ea8586cd47257741
Pablo Leonarduzzi <pablo.leonarduzzi@despegar.com>
2022-11-05 17:34:16 -0300

    inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Grid, Typography, Card, CardContent } from '@mui/material';
+import { Container, Grid, Typography, Card, CardContent, TextField } from '@mui/material';
 import { HorizontalGridLines, LineSeries, makeWidthFlexible, VerticalGridLines, XAxis, XYPlot, YAxis } from "react-vis";
 import * as math from 'mathjs';
 
@@ -16,24 +16,39 @@ function App() {
             Integración
           </Typography>
         </Grid>
-        <Grid item xs={12} md={8}>
-          <Card>
-            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-              <FlexibleXYPlot height={600}>
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <LineSeries
-                  color="red"
-                  data={[
-                    { x: 1, y: 10 },
-                    { x: 2, y: 5 },
-                    { x: 3, y: 15 }
-                  ]} />
-              </FlexibleXYPlot>
-            </CardContent>
-          </Card>
+        <Grid item xs={12}>
+          <Grid item xs={8}>
+            <Card>
+              <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                <FlexibleXYPlot height={600}>
+                  <VerticalGridLines />
+                  <HorizontalGridLines />
+                  <XAxis />
+                  <YAxis />
+                  <LineSeries
+                    color="red"
+                    fill="false"
+                    data={[
+                      { x: 1, y: 10 },
+                      { x: 2, y: 5 },
+                      { x: 3, y: 1 }
+                    ]} />
+                    <LineSeries
+                    data={[
+                      {x: 1, y: 12},
+                      {x: 2, y: 21},
+                      {x: 3, y: 2}
+                    ]}/>
+                </FlexibleXYPlot>
+              </CardContent>
+              </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <TextField id="function" label="Función" variant="outlined" />
+            <TextField id="width-a" label="a" variant="outlined" />
+            <TextField id="width-b" label="b" variant="outlined" />
+            <TextField id="n" label="n" variant="outlined" />
+          </Grid>
         </Grid>
       </Grid>
     </Container >
